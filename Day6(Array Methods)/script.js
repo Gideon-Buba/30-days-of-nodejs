f837ff65a765c1189205394fd82163f78ab69237
Gideon Buba <user@users-MacBook-Pro.local>
2023-12-24 21:51:51 +0100

    Started with array methods

---
+++ Day6(Array Methods)/script.js
@@ -0,0 +1,44 @@
+// An array of names
+// const names = ['Gideon', 'Courage', 'Jude', 'Psalm'];
+// console.log(names)
+
+// calculating the length of an array 
+// const len = names.length;
+// console.log(len)
+
+// Another way to create a new array 
+// const arr = new Array(3);
+// arr [0] = 'first element'
+// arr [1] = 'second element'
+// arr [2] = 'third element'
+// arr [3] = 'fourth element'
+
+// console.log(arr)
+
+// push() methods in arrays
+// const vowels = ['a', 'e', 'i', 'o'];
+// console.log(vowels)
+// vowels.push("u");
+// console.log(vowels);
+
+// pop() method in arrays 
+// it is used to remove the last element from an array 
+// const numbers = [2, 4, 6, 8];
+// console.log(numbers);
+// numbers.pop();
+// console.log(numbers);
+
+// unshift() method in arrays 
+// It is used to add an element to the begining of an array 
+// const ages = [ 12, 14, 16, 18, 20];
+// console.log(ages);
+// ages.unshift(10);
+// console.log(ages);
+
+// shift() method in arrays
+// it is used to remove the first element in an array 
+const evenNumbers = [1, 2, 4, 6, 8, 10];
+console.log(evenNumbers);
+evenNumbers.shift();
+console.log(evenNumbers);
+
