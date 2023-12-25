33b751056c12a54babf2e36e932e60d9af370511
Gideon Buba <user@users-MacBook-Pro.local>
2023-12-25 22:14:47 +0100

    completed day 6

--- Day6(Array Methods)/script.js
+++ Day6(Array Methods)/script.js
@@ -37,10 +37,61 @@
 
 // shift() method in arrays
 // it is used to remove the first element in an array 
-const evenNumbers = [1, 2, 4, 6, 8, 10];
-console.log(evenNumbers);
-evenNumbers.shift();
-console.log(evenNumbers);
+// const evenNumbers = [1, 2, 4, 6, 8, 10];
+// console.log(evenNumbers);
+// evenNumbers.shift();
+// console.log(evenNumbers);
 
 // reverse() method in arrays
+// const oddNumbers = [1, 3, 5, 7, 9];
+// console.log(oddNumbers);
+// oddNumbers.reverse();
+// console.log(oddNumbers);
+
+// sort() method in arrays
+// const names = ['Gideon', 'Jude', 'Courage', 'Psalm', 'Shekinah'];
+// console.log(names);
+// names.sort();
+// console.log(names);
+
+// splice() method for adding in arrays 
+// const fruits = ["apple", "Banana", "Cherry"];
+// console.log(fruits);
+// fruits.splice(3,4, "Orange", "lemon")
+// console.log(fruits)
+
+
+// splice() method for removing from arrays
+// const numbers = [1, 2, 3, 4, 5];
+// console.log(numbers);
+// numbers.splice(3,3);
+// console.log(numbers)
+
+// concat() meethod in arrays
+// const oddNumbers = [1, 3, 5, 7, 9];
+// const evenNumbers = [2, 4, 6, 8, 10];
+
+// const sortedNumbers = oddNumbers.concat(evenNumbers);
+
+// sortedNumbers.sort((a, b) => a - b);
+
+// console.log(sortedNumbers);
+
+
+// indexOf() method in arrays
+// const names = ['Gideon', 'Jude', 'Courage', 'Psalm', 'Shekinah'];
+// console.log(names);
+
+// const psalm = names.indexOf('Psalm');
+// console.log(`Index of psalm is ${psalm}`)
+
+// lastIndexOf() method in arrays 
+
+const names = ['Gideon', 'Jude', 'Courage', 'Psalm', 'Shekinah', 'Gideon', 'Courage']; 
+console.log(names);
+const position = names.lastIndexOf('Femi');
+const great = names.join('>')
+console.log(great)
+console.log(position);
+
 
