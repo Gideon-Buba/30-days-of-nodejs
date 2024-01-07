6c65ab69dd89c5a1274a0b27d853804972796158
Gideon Buba <bubaambore@gmail.com>
2024-01-07 22:13:44 +0100

    Getting to understand things

---
+++ Day12(Mongodb)/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const fs = require('fs');
+const app =  express();
+
+
+app.get('/', (req, res) => {
+    fs.readFile('./index.html', 'utf8', (err, html) => {
+        if (err) {
+            res.status(500).send("Sorry, an error has occured")
+        } else {
+         res.send(html)
+        } 
+    })     
+})
+
+app.get('/favicon.ico', (req, res) => res.status(204));
+
+
+client.connect().then(() => {
+    app.listen(3000, () => {
+    
+        console.log('Server is listening on port 3000');
+    });
+})
