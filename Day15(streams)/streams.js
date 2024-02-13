7a421db8526d95ba8bad5e27b9b60c0135065d04
Gideon-Buba <bubaambore@gmail.com>
2024-02-13 23:02:20 +0100

    Done with streams

--- Day15(streams)/streams.js
+++ Day15(streams)/streams.js
@@ -2,23 +2,15 @@ const fs = require('fs');
 const http = require("http");
 
 // Create a readable stream to read from a file
-const myReadStream = fs.createReadStream("readme.txt", "utf-8");
 
-// Create a writable stream to write to a file
-const myWriteStream = fs.createWriteStream("writeme.txt", { encoding: "utf-8" });
-
-// Pipe the readable stream to the writable stream to write to "writeme.txt"
-myReadStream.pipe(myWriteStream);
-
-// Create an HTTP server
+// create Http server 
 const server = http.createServer((req, res) => {
-    console.log('Request was made: ' + req.url);
-
-    // Set the response headers
+	console.log('Request was made: ' + req.url);	
     res.writeHead(200, {'Content-Type': 'text/plain'});
-
-    // Pipe the readable stream to the response stream to send the contents of "readme.txt"
+	
+	const myReadStream = fs.createReadStream("readme.txt", "utf-8");
     myReadStream.pipe(res);
+
 });
 
 // Listen to the server on port 3000
