10f93ce926ce22f12fd2e2eda8eff0d0d2126bd9
Gideon-Buba <bubaambore@gmail.com>
2024-02-21 20:40:28 +0100

    completed day 20

---
+++ Day20(Timers)/setTimeout.js
@@ -0,0 +1,6 @@
+// setTimeout()
+function hello() {
+  console.log("This will run after 2 seconds");
+}
+console.log("It will Print the data once after the delay of 2000ms");
+setTimeout(hello, 2000);
